9f76cac035a763716539d153d112330ad05346f6
Bohdan Shcherbak <b.royal1903@gmail.com>
2025-07-08 19:29:27 +0300

    Add Unity project files

---
+++ Assets/PipeSpawnScript.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PipeSpawnScript : MonoBehaviour
+{
+
+    public GameObject pipe;
+    public float spawnRate = 2;
+    private float timer = 0;
+    public float heightOffset = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spwanPipe();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer < spawnRate)
+        {
+            timer = timer + Time.deltaTime;
+        }
+        else
+        {
+            spwanPipe();
+            timer = 0;
+        }
+    }
+
+    void spwanPipe()
+    {
+        float lowestPoint = transform.position.y - heightOffset;
+        float heighestPoint = transform.position.y + heightOffset;
+
+        Instantiate(pipe, new Vector3(transform.position.x, Random.Range(lowestPoint, heighestPoint), 0), transform.rotation);
+    }
+}
